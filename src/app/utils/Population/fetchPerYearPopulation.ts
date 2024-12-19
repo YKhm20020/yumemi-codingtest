c259714a912c95a4c8d73e83825dee94328db67b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-19 20:31:10 +0900

    add: グラフ描画に必要なので、年ごとの人口を取得する関数を作成

---
+++ src/app/utils/Population/fetchPerYearPopulation.ts
@@ -0,0 +1,29 @@
+export const fetchPerYearPopulation = async () => {
+    const url = process.env.RESAS_API_URL || '';
+    const apiKey = process.env.RESAS_API_KEY || '';
+
+    try {
+        const response = await fetch(
+            `${url}/api/v1/population/composition/perYear?prefCode=1`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': apiKey,
+                },
+            },
+        );
+
+        const json = await response.json();
+        // ここのデータはちゃんと来てそう
+        console.log(json);
+
+        if (json?.data?.errorMessage) {
+            throw new Error(json.data.errorMessage);
+        }
+
+        return json;
+    } catch (error) {
+        throw new Error(error?.toString() || 'Population データの取得に失敗しました');
+    }
+};
