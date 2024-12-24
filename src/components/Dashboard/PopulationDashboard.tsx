b3c244c81e979f74dc02eb86976cb21706742d77
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-24 14:36:04 +0900

    update: ダッシュボードで管理するようにしたチェックボックスのチェック状態をグラフに反映させるため、仮の配列を渡していたものを、実際の選択された都道府県コードの配列を渡すように変更

--- src/components/Dashboard/PopulationDashboard.tsx
+++ src/components/Dashboard/PopulationDashboard.tsx
@@ -1,11 +1,21 @@
+'use client';
+
 import { PopulationChart } from '@/components/Graph/PopulationGraph';
 import { CheckBoxList } from '@/components/List/CheckBoxList';
+import { useState } from 'react';
 
 export const PopulationDashboard = () => {
+    const [selectedPrefCodes, setSelectedPrefCodes] = useState<string[]>([]);
+
+    // CheckBoxList から選択された都道府県コードを受け取る
+    const handlePrefCodeChange = (prefCodes: string[]) => {
+        setSelectedPrefCodes(prefCodes);
+    };
+
     return (
         <div>
-            <CheckBoxList />
-            <PopulationChart prefCodes={['1', '2', '3']} dataType={1} />
+            <CheckBoxList onChange={handlePrefCodeChange} />
+            <PopulationChart prefCodes={selectedPrefCodes} dataType={1} />
         </div>
     );
 };
