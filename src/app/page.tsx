bec67f06968c8f308d4eecb377ed89d2ca811912
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-24 14:01:23 +0900

    update: 仮作成したダッシュボードのモックをページに配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { PopulationChart } from '@/components/Graph/PopulationGraph';
-import { CheckBoxList } from '@/components/List/CheckBoxList';
+import { PopulationDashboard } from '@/components/Dashboard/PopulationDashboard';
 
 export default function Home() {
     return (
         <main className='container mx-auto p-4'>
             <h1 className='text-2xl font-bold mb-4'>都道府県別人口推移</h1>
-            <CheckBoxList />
-            <PopulationChart prefCode='1' dataType={1} />
+            <PopulationDashboard />
         </main>
     );
 }
