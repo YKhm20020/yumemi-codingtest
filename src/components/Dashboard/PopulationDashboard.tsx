bfc20c1d3d13edd7a074c623fcd5a1053887047b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-26 03:27:13 +0900

    update: 作成したドロップダウンリストで選択した人口種別のデータをグラフで表示できるよう、仮で連携してコンポーネントを配置

--- src/components/Dashboard/PopulationDashboard.tsx
+++ src/components/Dashboard/PopulationDashboard.tsx
@@ -1,22 +1,42 @@
 'use client';
 
+import { CommonDropDown } from '@/components/DropDown/CommonDropDown';
 import { PopulationGraph } from '@/components/Graph/PopulationGraph';
 import { CheckBoxList } from '@/components/List/CheckBoxList';
 import type { PrefectureData } from '@/types/population';
+import { PopulationTypeLabels, PopulationTypes } from '@/types/population/populationType';
 import { useState } from 'react';
+
 export const PopulationDashboard = () => {
     // 選択された都道府県データの配列を監視
     const [selectedPrefectures, setSelectedPrefectures] = useState<PrefectureData[]>([]);
+    const [populationType, setPopulationType] = useState<number>(PopulationTypes.TOTAL);
+
+    // 人口種別のオプションを作成
+    const populationOptions = Object.entries(PopulationTypeLabels).map(([value, label]) => ({
+        value: Number(value),
+        label,
+    }));
 
     // CheckBoxList から選択された都道府県コードを受け取る
     const handlePrefectureChange = (prefectureData: PrefectureData[]) => {
         setSelectedPrefectures(prefectureData);
     };
 
+    // 人口種別の選択が変更された場合に呼び出される関数
+    const handlePopulationTypeChange = (value: string | number) => {
+        setPopulationType(Number(value));
+    };
+
     return (
         <div>
+            <CommonDropDown
+                label='人口種別'
+                options={populationOptions}
+                onChange={handlePopulationTypeChange}
+            />
             <CheckBoxList onChange={handlePrefectureChange} />
-            <PopulationGraph prefectureData={selectedPrefectures} dataType={1} />
+            <PopulationGraph prefectureData={selectedPrefectures} dataType={populationType} />
         </div>
     );
 };
