f9e98af98ebb5864be86ca0c7dd7ab5b2cf7166c
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-31 19:45:24 +0900

    add: 汎用的なドロップダウンのテストを追加

--- __test__/components/Dropdown/CommonDropdown.test.tsx
+++ __test__/components/Dropdown/CommonDropdown.test.tsx
@@ -1,6 +1,6 @@
 import { CommonDropdown } from '@/components/Dropdown/CommonDropdown';
-import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { mockPopulationType } from '../../mocks/population/populationType';
 import '@testing-library/jest-dom/vitest';
 
@@ -8,22 +8,29 @@ describe('CommonDropdown', () => {
     // 人口種別モックデータの読み込み
     const options = mockPopulationType;
 
-    // ドロップダウンメニューのレンダリング
-    const content = render(
-        <CommonDropdown dropdownLabel='ドロップダウン' options={options} onChange={() => {}} />,
-    );
+    // テストごとにレンダリング
+    beforeEach(() => {
+        render(
+            <CommonDropdown dropdownLabel='ドロップダウン' options={options} onChange={() => {}} />,
+        );
+    });
+
+    // テストごとにレンダリングをクリア
+    afterEach(() => {
+        cleanup();
+    });
 
     it('ドロップダウンが正しく表示される', () => {
         // ドロップダウンラベルの確認
-        expect(content.getByLabelText('ドロップダウン')).toBeInTheDocument();
+        expect(screen.getByLabelText('ドロップダウン')).toBeInTheDocument();
 
         // オプションの確認
         for (const option of options) {
-            expect(content.getByText(option.label)).toBeInTheDocument();
+            expect(screen.getByText(option.label)).toBeInTheDocument();
         }
     });
 
     it('選択肢は全部で4つである', () => {
-        expect(content.getAllByRole('option')).toHaveLength(4);
+        expect(screen.getAllByRole('option')).toHaveLength(4);
     });
 });
